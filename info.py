d325e4252a2eea2c3e140756010311333fa12b0b
Anantha Ramu. K <142468482+anantharamu15@users.noreply.github.com>
2025-06-09 13:31:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5
 
 # Admins, Channels & Users
 USERNAME = environ.get("USERNAME", "https://t.me/Anand_2K")
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1735392935 5244510268 ').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1735392935').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002144697861').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
